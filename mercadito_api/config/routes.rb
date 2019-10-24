04b0d3b278ef67ab80a8128eed2879e10fc9138d
Roberto Garcia <croberton7@gmail.com>
2019-10-24 17:44:05 -0500

    Set versioning namespaces for aPI

--- mercadito_api/config/routes.rb
+++ mercadito_api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json } do
 
-    namespace: :v1 do
+    namespace :v1 do
       #We are going to list our resources here
 
     end
