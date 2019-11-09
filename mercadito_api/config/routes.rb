4845cadf67b31a45e9cef2e74ee2fe1fbf2e43d3
Roberto Garcia <croberton7@gmail.com>
2019-11-08 21:02:11 -0600

    Setup token controllers

--- mercadito_api/config/routes.rb
+++ mercadito_api/config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   # API definition
   namespace :api, defaults: { format: :json } do
 
     namespace :v1 do
       #We are going to list our resources here
-      resources :users, only: %i[show create update destroy]
+      #resources :users, only: %i[show create update destroy]
+      resources :tokens, only: [:create]
 
     end
 
